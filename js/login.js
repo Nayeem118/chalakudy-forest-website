55e18a43a4ee507a7f329022a3fc62afc9d5cdb1
Nayeem118 <149505919+Nayeem118@users.noreply.github.com>
2026-05-18 11:19:04 +0530

    project structure changes
    create separate folders for css,js and pages
    move current css,js file to new folder
    create login page

---
+++ js/login.js
@@ -0,0 +1,11 @@
+console.log("Department Login Page Loaded");
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Login system backend will be connected later using Django.");
+
+});
